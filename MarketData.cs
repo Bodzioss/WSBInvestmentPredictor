4e44c97d2518b54a1f8c65897a365d9030f30b64
Marcin Bogdański <marcin_bogdanski1@wp.pl>
2025-03-29 11:50:48 +0100

    Add MarketData class for data input

---
+++ MarketData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+namespace AIInvestmentPredictor
+{
+    public class MarketData
+    {
+        // Właściwości wejściowe (cechy modelu)
+        [LoadColumn(1)] public float Open { get; set; }
+        [LoadColumn(2)] public float High { get; set; }
+        [LoadColumn(3)] public float Low { get; set; }
+        [LoadColumn(4)] public float Close { get; set; }
+        [LoadColumn(5)] public float AdjClose { get; set; }
+        [LoadColumn(6)] public float Volume { get; set; }
+
+        // Zmienna docelowa (np. przyszła cena)
+        // Można np. prognozować Close z przesunięciem (t+5, t+30 itd.)
+        [LoadColumn(7)] public float Target { get; set; }
+    }
+}
