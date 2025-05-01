ac6448882905104ee1fe824d7189fa52703f1e17
Marcin Bogdański <m.bogdanski@sente.pl>
2025-05-01 21:58:41 +0200

    Add basic unit tests

--- Backend/src/Prediction/WSBInvestmentPredictor.Prediction.API/Program.cs
+++ Backend/src/Prediction/WSBInvestmentPredictor.Prediction.API/Program.cs
@@ -1,15 +1,17 @@
+using FluentValidation;
 using MediatR;
 using WSBInvestmentPredictor.Prediction.Application.Commands;
 using WSBInvestmentPredictor.Prediction.Application.Common.Behaviors;
 using WSBInvestmentPredictor.Prediction.Application.FeatureEngeneering;
 using WSBInvestmentPredictor.Prediction.Application.Validators;
+using WSBInvestmentPredictor.Prediction.Domain.Interfaces;
 using WSBInvestmentPredictor.Predictor.Infrastructure.Prediction;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(); builder.Services.AddControllers();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -25,7 +27,7 @@ builder.Services.AddValidatorsFromAssembly(typeof(TrainModelCommandValidator).As
 // Rejestracja globalnego pipeline do walidacji
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
-builder.Services.AddSingleton<StockPredictorService>();
+builder.Services.AddScoped<IStockPredictorService, StockPredictorService>();
 builder.Services.AddSingleton<MarketDataBuilder>();
 
 var app = builder.Build();
