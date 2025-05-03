b148a82374a9acfac17fe0af74072e100f0146c6
Marcin Bogdański <m.bogdanski@sente.pl>
2025-05-03 08:43:00 +0200

    Basic frontend project structure

---
+++ Frontend/src/Prediction/WSBInvestmentPredictor.Prediction/Services/PredictionService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WSBInvestmentPredictor.Prediction.Models;
+
+namespace WSBInvestmentPredictor.Prediction.Services;
+
+public class PredictionService
+{
+    private readonly HttpClient _http;
+
+    public PredictionService(HttpClient http) => _http = http;
+
+    public async Task<PredictionResultDto?> PredictAsync(MarketDataInput input)
+    {
+        var response = await _http.PostAsJsonAsync("api/prediction/predict", input);
+        return await response.Content.ReadFromJsonAsync<PredictionResultDto>();
+    }
+}
